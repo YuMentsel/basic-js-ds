d34f11e4abc033d7df7ec9c0887173eb80d14542
YuMentsel <101459634+YuMentsel@users.noreply.github.com>
2022-08-17 01:15:39 +0300

    fix

--- src/stack.js
+++ src/stack.js
@@ -12,20 +12,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
-class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ 
+class Stack {  
+
+  stackList = [];
+
+  push(element) {    
+    return this.stackList.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stackList.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stackList.at(-1);
   }
 }
 
